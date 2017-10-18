4feba3ed9612dc2993c0c45871546ce9389b029b
MacBook Pro <apple@MacBook-Pro-de-MacBook.local>
2017-10-18 12:40:33 +0200

    Add CORS Headers

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ const config = require('./config');
 
 // Create Express webapp
 const app = express();
+
+// Add headers
+app.all('/*', function(req, res, next) {
+    // CORS headers
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Access-Control-Allow-Origin');
+    res.header("Access-Control-Max-Age", "86400"); // 24 hours
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 
