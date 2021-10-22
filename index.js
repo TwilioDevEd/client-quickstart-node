7239d0174596bdb9e1f08cd87101a3159eaebfad
“Alexis <alexis-2cab@hotmail.com>
2021-10-22 16:17:04 -0500

    Add error handler, add NODE_ENV to .env.example file, update package.lock.json

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ app.use(bodyParser.json());
 
 app.use(router);
 
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
+
 // Create http server and run it
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
