e9e128c9242291834255e833ebe34872b245ed51
Alexis Crespo <alexis-2cab@hotmail.com>
2021-10-28 08:19:32 -0500

    Update default behavior to production environment settings

--- index.js
+++ index.js
@@ -13,25 +13,12 @@ app.use(bodyParser.json());
 
 app.use(router);
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-    });
-  });
-}
-
-// production error handler
+// error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  console.trace(err);
   res.status(err.status || 500);
-  res.render('error', {
+  res.send({
     message: err.message,
     error: {},
   });
